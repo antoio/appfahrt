56fbb36674eca98169403a924798403c3c0e27da
antonio galluccio <antonio.galluccio@gmail.com>
2018-09-25 22:49:55 +0200

    removed angular maps dependency

--- appfahrt/src/app/map-view/map-view.component.ts
+++ appfahrt/src/app/map-view/map-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import {  } from '@types/googlemaps';
 
 @Component({
   selector: 'app-map-view',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapViewComponent implements OnInit {
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
