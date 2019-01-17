9b6752c66c8a36d31cf9e3538cd6b75d7150b9dd
antonio galluccio <antonio.galluccio@gmail.com>
2019-01-17 22:42:36 +0100

    add login error field

--- appfahrt/src/app/login/login.component.ts
+++ appfahrt/src/app/login/login.component.ts
@@ -1,22 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthServiceService} from '../services/auth-service.service';
-import {FormBuilder, FormGroup, Validators, FormControl, ReactiveFormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../services/auth-service.service';
+import { FormBuilder, FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('fadeOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate('1s')),
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   loading = false;
   submitted = false;
+  error: boolean = false;
 
   constructor(private authService: AuthServiceService,
-              private router: Router,
-              private formBuilder: FormBuilder) {
+    private router: Router,
+    private formBuilder: FormBuilder) {
     // if (this.authService.isLoggedIn) {
     //   console.log('Already logged in. Navigate to home.');
     //   this.router.navigate(['/']);
@@ -43,6 +53,7 @@ export class LoginComponent implements OnInit {
         console.log('Login worked too 😃');
       })
       .catch((err) => {
+        this.error = err;
         console.log(err);
       });
   }
