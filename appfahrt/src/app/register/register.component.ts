becb4a1cf1ba70348219896dd3ff709198c371fb
antonio galluccio <antonio.galluccio@gmail.com>
2019-01-20 10:26:14 +0100

    add error handling to user registration

--- appfahrt/src/app/register/register.component.ts
+++ appfahrt/src/app/register/register.component.ts
@@ -3,16 +3,26 @@ import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthServiceService} from '../services/auth-service.service';
 import {DatabaseService} from '../services/database-service.service';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  animations: [
+    trigger('fadeOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate('1s')),
+    ])
+  ]
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   loading = false;
   submitted = false;
+  error: boolean = false;
 
   constructor(private authService: AuthServiceService,
               private router: Router,
@@ -43,7 +53,10 @@ export class RegisterComponent implements OnInit {
         this.databaseService.addNearestFavorite(res.user.uid);
       })
       .catch((err) => {
+        this.error = err;
         console.log(err);
+        //reset the error message
+        setTimeout(() => {this.error = null}, 7000);
       });
   }
 
